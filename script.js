bfb8329058e7c9e325ce9bfcb0570499b592897e
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2025-03-13 13:12:18 +0100

    commit om javascript kod och lite css inheritance (arv)

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function lägg_till_korg(){
     console.log("Du har lagt till en produkt till varukorgen")
 }
 
+let totala_priset = 0;
+
+
 function Lägg_till_Varukorg_Knapp(){
     let cart = document.querySelector(".Kundvagn-låda")
 }
