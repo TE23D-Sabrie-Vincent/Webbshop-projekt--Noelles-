512e43b26f2a8aeab649689b815b6c93cf265794
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2025-03-13 07:58:53 +0100

    Påbörjade Javascript kod och experimenterar fram

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function lägg_till_korg(){
     console.log("Du har lagt till en produkt till varukorgen")
 }
+
+function Lägg_till_Varukorg_Knapp(){
+    let cart = document.querySelector(".Kundvagn-låda")
+}
+
+let knappar = document.querySelectorAll(".Produkt-varukorg");
